a7a8c19e86c67053c60d51dbd532cd107fe95086
ouyangchao <ouyangchao@bytedance.com>
2020-05-02 14:38:20 +0800

    feat: hello world

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import { parse } from '@creditkarma/thrift-parser';
 import chalk from 'chalk';
 
 const main = () => {
-  process.stdout.write(chalk.yellow('hello cli'));
+  process.stdout.write(chalk.yellow('hello cli\n'));
+  console.log(parse('struct EmptyReq {}'));
 };
 
 export default main;
