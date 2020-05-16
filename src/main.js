a208d4e4473e4d8d65a94d416898b9ddf30a7fde
ouyangchao <ouyangchao@bytedance.com>
2020-05-16 08:48:32 +0800

    chore: read thrift file

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
 
+const INPUT_FILE = path.resolve(__dirname, '..', 'input', 'demo.thrift');
+const OUTPUT_DEST = path.resolve(__dirname, '..', 'output');
+
 const main = async () => {
   process.stdout.write(chalk.yellow('hello cli\n'));
-  const ast = parse('struct EmptyReq {}');
-  fs.promises.writeFile(path.resolve(__dirname, '..', 'output', 'ast.json'), JSON.stringify(ast, null, 2));
+
+  const thriftCode = (await fs.promises.readFile(INPUT_FILE)).toString();
+  const thriftAst = parse(thriftCode);
+
+  fs.promises.writeFile(path.resolve(OUTPUT_DEST, 'ast.json'), JSON.stringify(thriftAst, null, 2));
 };
 
 export default main;
